3a174a65fe9572ca4c98cf2f426f11f9f3356336
Christopher <kriss17388+dev@gmail.com>
2023-08-27 12:26:12 +0100

    stitcher is working locally

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+
+one = "one.mp3"
+two = "two.mp3"
+three = "three.mp3"
 
 app = FastAPI()
 
+def stitcher():
+    with open(one, mode="rb") as file_like:
+        yield from file_like
+    with open(two, mode="rb") as file_like:
+        yield from file_like
+    with open(three, mode="rb") as file_like:
+        yield from file_like
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/station")
+async def station():
+    stitcher()
+    return StreamingResponse(stitcher(), media_type="audio/mp3")
